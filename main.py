792a721270fa944cdfc2a89fa33cd77759aeafd5
Giacomo <giacomolawrance@gmail.com>
2021-02-11 12:01:34 +0000

    added main working body

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv # noqa
 from pushover import init, Client # noqa
+import feedparser
+from time import time, sleep
 
 
 # load .env
@@ -11,4 +13,20 @@ load_dotenv(dotenv_path)
 usertoken = os.getenv("USERTOKEN")
 token = os.getenv("TOKEN")
 init(token)
-Client(usertoken).send_message("Hello!", title="Hello")
+
+while True:
+	print('done')
+	sleep(10 - time() % 10)
+	with open('rsslist.txt') as f:
+		print('doing')
+		for line in f:
+			d = feedparser.parse(line)
+			feed = feedparser.parse(line, modified=d.modified)
+			if feed.status == 304:
+				break
+			else:
+				for key in feed["entries"]:
+					title = key['title']
+					url = key['links'][0]['href']
+				print(title, url)
+				Client(usertoken).send_message(url, title=title)
